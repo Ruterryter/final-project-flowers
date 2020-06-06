220074e2798e3c95441e9c7eaa7d3702f8cff43c
Ruterryter <hanna.ruter@gmail.com>
2020-06-06 15:36:37 +0200

    added thunk, fecthing data from database

--- frontend/src/components/Products.js
+++ frontend/src/components/Products.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { useSelector } from "react-redux"
 import { Product } from "components/Product"
+import { createSlice } from '@reduxjs/toolkit'
 
-//Lägga till fetch från databasen istället?
-//dispatch a thunk here
-//send links with info 
+
 
 export const Products = () => {
-
   const allProducts = useSelector((store) => store.products)
 
   return (
--- frontend/src/reducers/products.js
+++ frontend/src/reducers/products.js
@@ -1,14 +1,51 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const productData = [
-  { "_id": { "$oid": "5ed611a410b33c002372376b" }, "name": "finaste2", "price": 550, "description": "fina blommor", "imageUrl": "https://www.interflora.se/siteassets/produkter/1201585-2.jpg", "__v": 0 },
-  { "_id": { "$oid": "5ed63b8f4ff4e40023fbf1d9" }, "name": "studentblomma", "price": 750, "description": "fina sommarblommor", "imageUrl": "https://www.interflora.se/siteassets/produkter/1201709.jpg", "__v": 0 },
-  { "_id": { "$oid": "5ed63c824ff4e40023fbf1da" }, "name": "Pioner", "price": 450, "description": "fina rosa pioneer", "imageUrl": "https://www.interflora.se/siteassets/produkter/1201224.jpg", "__v": 0 }
-]
+// const productData = [
+//   { "_id": { "$oid": "5ed611a410b33c002372376b" }, "name": "finaste2", "price": 550, "description": "fina blommor", "imageUrl": "https://dl.dropboxusercontent.com/s/sdq16xthwcn4gph/mediumBukett1.jpg?dl=0", "__v": 0 },
+//   { "_id": { "$oid": "5ed63b8f4ff4e40023fbf1d9" }, "name": "studentblomma", "price": 750, "description": "fina sommarblommor", "imageUrl": "https://dl.dropboxusercontent.com/s/sdq16xthwcn4gph/mediumBukett1.jpg?dl=0", "__v": 0 },
+//   { "_id": { "$oid": "5ed63c824ff4e40023fbf1da" }, "name": "Pioner", "price": 450, "description": "fina rosa pioneer", "imageUrl": "https://dl.dropboxusercontent.com/s/sdq16xthwcn4gph/mediumBukett1.jpg?dl=0", "__v": 0 }
+// ]
 
-//fetch from database? here?
+//https://bouquetdb.herokuapp.com/bouquets
+//(osäker på om denna är rätt, behöver jag skicka in något mer? så att den får tillgång till all data.  )
 
 export const products = createSlice({
   name: "products",
-  initialState: productData
+  initialState: [],
+  reducers: {
+    setProduct: (state, action) => {
+      const { productData } = action.payload;
+      state.products.productData = productData;
+    }
+  },
+  setErrorMessage: (state, action) => {
+    const { errorMessage } = action.payload;
+    state.products.errorMessage = errorMessage;
+  }
 })
+
+//Thunk (osäker på om denna är rätt, behöver jag skicka in något mer? )
+export const bouquets = (name, price) => {
+  const FLOWER_URL = "https://bouquetdb.herokuapp.com/bouquets"
+  return (dispatch) => {
+    fetch(FLOWER_URL, {
+      method: "GET",
+    })
+      .then((res) => {
+        if (res.ok /* if 200, 201, 204 */) {
+          return res.json();
+        }
+        throw 'Could not show any products, please refresh page.';
+      })
+      .then((json) => {
+        dispatch(
+          products.actions.setProduct({ setProduct: JSON.stringify(json) }));
+      })
+      .catch((err) => {
+
+        dispatch(products.actions.setErrorMessage({ errorMessage: err }));
+      }); //401
+  };
+};
+
+
