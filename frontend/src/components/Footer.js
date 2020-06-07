e7fa89d8c4f3aee79be14258e43be93ab5c22fae
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-06-07 14:51:48 +0200

    Content & responsive footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,13 +6,49 @@ const FooterDiv = styled.div`
   width: 100%;
   height: 300px;
   display: flex;
-  flex-direction: column;
-  flex-wrap: wrap;
+  flex-flow: row nowrap;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
   background-color: #ff7c98;
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    justify-content: center;
+  }
+`;
+
+const Title = styled.h1`
+  font-family: "Poppins";
+  font-size: 42px;
+  font-weight: 700;
+  text-transform: uppercase;
+  text-align: center;
+  color: #fde7ea;
+`;
+
+const Link = styled.a`
+  margin-right: 10px;
+  font-family: "Poppins";
+  font-size: 18px;
+  font-weight: 400;
+  text-transform: uppercase;
+  text-align: center;
+  color: #fde7ea;
+
+  :hover {
+    color: orange;
+  }
 `;
 
 export const Footer = () => {
-  return <FooterDiv></FooterDiv>;
+  return (
+    <FooterDiv>
+      <Title>Friday flowers</Title>
+      <div>
+        <Link>About Us</Link>
+        <Link>Contact Us</Link>
+        <Link>Log In</Link>
+      </div>
+    </FooterDiv>
+  );
 };
