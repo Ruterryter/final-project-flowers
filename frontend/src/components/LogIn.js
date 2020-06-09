dac495697e923526d0061a5a5b7c36b602889448
Ruterryter <hanna.ruter@gmail.com>
2020-06-09 10:57:10 +0200

    fixed confirmation after checkout

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -3,11 +3,8 @@ import styled from "styled-components/macro";
 import { Profile } from "./Profile";
 import { useDispatch, useSelector } from "react-redux";
 import { user, login, logout } from "../reducers/user";
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
-=======
 import { ConfirmationPage } from "components/ConfirmationPage"
->>>>>>> hanna_dev
 
 const LOGIN_URL = "https://bouquetdb.herokuapp.com/sessions";
 
@@ -60,6 +57,17 @@ const LoginContainer = styled.div`
   width: 45%;
 `;
 
+const Button = styled.button`
+  background-color: #ff7c98;
+  color: #ffff;
+  font-family: "Poppins", sans-serif;
+  font-weight: 700;
+  text-transform: uppercase;
+  outline: none;
+  border: none;
+  margin: 20px;
+`;
+
 export const LogIn = () => {
   const dispatch = useDispatch();
   const accessToken = useSelector((store) => store.user.login.accessToken);
@@ -75,7 +83,6 @@ export const LogIn = () => {
 
   if (!accessToken) {
 
-    /* //Lägg till signin länk */
     return (
       <Wrapper>
         {errorMessage && <h4>Error Message : {`${errorMessage}`}</h4>}
@@ -98,7 +105,7 @@ export const LogIn = () => {
               value={password}
               onChange={(event) => setPassword(event.target.value)}
             />
-            <Link
+            {/* <Link
               to={"/"}
               style={{
                 backgroundColor: "#ff7c98",
@@ -112,7 +119,12 @@ export const LogIn = () => {
               }}
             >
               Logga in
-            </Link>
+            </Link> */}
+
+            <Button type="submit" onClick={handleLogin}>
+              Logga in
+            </Button>
+
             <Link
               to={"/signup/"}
               style={{
