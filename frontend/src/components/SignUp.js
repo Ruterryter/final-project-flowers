6c2cac1ac382bde164bf64bb765adc24d9269aa5
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-06-14 11:20:01 +0200

    Styling Signup and media queries added

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -1,17 +1,31 @@
-import React, { useState } from "react"
-import styled from "styled-components/macro"
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from "react";
+import styled from "styled-components/macro";
+import { useDispatch, useSelector } from "react-redux";
 import { user, login, signup } from "../reducers/user";
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
+import { NavBar } from "components/Navbar";
+import { Footer } from "components/Footer";
+import headerPic from "../assets/Headerpic.jpeg";
 
-const SIGNUP_URL = "https://bouquetdb.herokuapp.com/users"
+const SIGNUP_URL = "https://bouquetdb.herokuapp.com/users";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${headerPic});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: pink;
+`;
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-`
+`;
 
 const Header = styled.header`
   font-family: "Poppins";
@@ -19,14 +33,14 @@ const Header = styled.header`
   font-weight: 700;
   text-transform: uppercase;
   text-align: center;
-  color: #FF7C98;
+  color: #ff7c98;
   margin-bottom: 20px;
-`
+`;
 
 const Label = styled.label`
   text-align: center;
-  color: #FF7C90;
-`
+  color: #ff7c90;
+`;
 
 const Input = styled.input`
   margin: 10px 0;
@@ -34,80 +48,103 @@ const Input = styled.input`
   border: none;
   font-family: "Poppins";
   font-size: 8px;
-  ::placeholder{
-  color: #FF7C90;
-  }: 
-`
+  ::placeholder {
+    color: #ff7c90;
+  }
+`;
 const StyledButton = styled.button`
   background-color: transparent;
-  color: #FF7C98;
-  font-family: 'Poppins', sans-serif;
+  color: #ff7c98;
+  font-family: "Poppins", sans-serif;
   font-weight: 700;
-  text-transform: uppercase; 
+  text-transform: uppercase;
   outline: none;
-  border: 2px solid #FF7C98;
+  border: 2px solid #ff7c98;
   margin: 20px;
-`
+`;
 
 const SignUpContainer = styled.div`
-  background-color: #FDE7EA;
+  background-color: #fde7ea;
   padding: 20px;
-  margin-bottom: 20px;
-  width: 45%;`
+  margin: 20px;
+  width: 45%;
+  opacity: 95%;
 
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
 
 export const SignUp = () => {
   const dispatch = useDispatch();
   const history = useHistory();
   const accessToken = useSelector((store) => store.user.login.accessToken);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [address, setAddress] = useState('');
-  const [zipCode, setZipCode] = useState('');
-  const [city, setCity] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [city, setCity] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const errorMessage = useSelector((store) => store.user.login.errorMessage);
 
-
-  // To sign up a user. 
+  // To sign up a user.
   const handleSignup = (event) => {
     event.preventDefault();
-    history.push()
+    history.push();
 
-    const userSignedUp = { email, password, firstName, lastName, address, city, zipCode, phoneNumber }
+    const userSignedUp = {
+      email,
+      password,
+      firstName,
+      lastName,
+      address,
+      city,
+      zipCode,
+      phoneNumber,
+    };
     fetch(SIGNUP_URL, {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(userSignedUp),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     })
       .then((res) => {
-        console.log("parsing")
+        console.log("parsing");
         if (!res.ok) {
-
-          throw 'Could not create account.  Try a different email address.';
+          throw "Could not create account.  Try a different email address.";
         }
         return res.json();
       })
       .then((json) => {
         // Save the login info
-        console.log("successful reg")
+        console.log("successful reg");
         dispatch(
           user.actions.setAccessToken({
             accessToken: json.accessToken,
           })
         );
         dispatch(user.actions.setUserId({ userId: json.userId }));
-        dispatch(userSignedUp(firstName, lastName, email, password, address, zipCode, city, phoneNumber))
-        setFirstName('')
-        setLastName('')
-        setEmail('')
-        setPassword('')
-        setAddress('')
-        setZipCode('')
-        setCity('')
-        setPhoneNumber('')
+        dispatch(
+          userSignedUp(
+            firstName,
+            lastName,
+            email,
+            password,
+            address,
+            zipCode,
+            city,
+            phoneNumber
+          )
+        );
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
+        setAddress("");
+        setZipCode("");
+        setCity("");
+        setPhoneNumber("");
       })
 
       .catch((err) => {
@@ -117,45 +154,93 @@ export const SignUp = () => {
   if (!accessToken) {
     return (
       <>
-        <SignUpContainer>
-          <Form>
-            {errorMessage && <h4>Error Message : {`${errorMessage}`}</h4>}
-            <Header>Skapa användare</Header>
-            <Label>Förnamn </Label>
-            <Input type="Text" required placeholder="Förnamn" value={firstName} onChange={(event) => setFirstName(event.target.value)} />
+        <NavBar />
+        <Wrapper>
+          <SignUpContainer>
+            <Form>
+              {errorMessage && <h4>Error Message : {`${errorMessage}`}</h4>}
+              <Header>Skapa användare</Header>
+              <Label>Förnamn </Label>
+              <Input
+                type="Text"
+                required
+                placeholder="Förnamn"
+                value={firstName}
+                onChange={(event) => setFirstName(event.target.value)}
+              />
 
-            <Label>Efternamn</Label>
-            <Input type="Text" required placeholder="Efternamn" value={lastName} onChange={(event) => setLastName(event.target.value)} />
+              <Label>Efternamn</Label>
+              <Input
+                type="Text"
+                required
+                placeholder="Efternamn"
+                value={lastName}
+                onChange={(event) => setLastName(event.target.value)}
+              />
 
-            <Label>Adress</Label>
-            <Input type="Text" required placeholder="Din gata" value={address} onChange={(event) => setAddress(event.target.value)} />
+              <Label>Adress</Label>
+              <Input
+                type="Text"
+                required
+                placeholder="Din gata"
+                value={address}
+                onChange={(event) => setAddress(event.target.value)}
+              />
 
-            <Label>Postnummer</Label>
-            <Input type="Number" required placeholder="123 45" value={zipCode} onChange={(event) => setZipCode(event.target.value)} />
+              <Label>Postnummer</Label>
+              <Input
+                type="Number"
+                required
+                placeholder="123 45"
+                value={zipCode}
+                onChange={(event) => setZipCode(event.target.value)}
+              />
 
-            <Label>Ort</Label>
-            <Input type="Text" required placeholder="Malmö" value={city} onChange={(event) => setCity(event.target.value)} />
+              <Label>Ort</Label>
+              <Input
+                type="Text"
+                required
+                placeholder="Malmö"
+                value={city}
+                onChange={(event) => setCity(event.target.value)}
+              />
 
-            <Label>Telefonnummer</Label>
-            <Input type="Number" placeholder="0701-100 10 10" value={phoneNumber} onChange={(event) => setPhoneNumber(event.target.value)} />
+              <Label>Telefonnummer</Label>
+              <Input
+                type="Number"
+                placeholder="0701-100 10 10"
+                value={phoneNumber}
+                onChange={(event) => setPhoneNumber(event.target.value)}
+              />
 
-            <Label>Email</Label>
-            <Input type="text" required placeholder="dittnamn@gmail.com" value={email} onChange={(event) => setEmail(event.target.value)} />
+              <Label>Email</Label>
+              <Input
+                type="text"
+                required
+                placeholder="dittnamn@gmail.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
 
-            <Label>Password</Label>
-            <Input type="password" required placeholder="********" vale={password} onChange={(event) => setPassword(event.target.value)} />
+              <Label>Password</Label>
+              <Input
+                type="password"
+                required
+                placeholder="********"
+                vale={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
 
-            <StyledButton type="submit" onClick={handleSignup}>Registrera dig</StyledButton>
-          </Form>
-        </SignUpContainer>
+              <StyledButton type="submit" onClick={handleSignup}>
+                Registrera dig
+              </StyledButton>
+            </Form>
+          </SignUpContainer>
+        </Wrapper>
+        <Footer />
       </>
-
-    )
+    );
   } else {
-    return (
-      <>
-
-      </>
-    )
+    return <></>;
   }
-}
+};
