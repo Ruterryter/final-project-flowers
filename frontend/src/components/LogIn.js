e74223965e27e5f2c183ee8e5e19de524b593e63
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-06-14 11:15:56 +0200

    Login page styled and mobile query added

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -4,18 +4,24 @@ import { Profile } from "./Profile";
 import { useDispatch, useSelector } from "react-redux";
 import { user, login, logout } from "../reducers/user";
 import { Link } from "react-router-dom";
-import { ConfirmationPage } from "components/ConfirmationPage"
+import { ConfirmationPage } from "components/ConfirmationPage";
 import { Checkout } from "./Checkout";
-import { OrderSummary } from "components/OrderSummary"
+import { OrderSummary } from "components/OrderSummary";
+import { NavBar } from "components/Navbar";
+import { Footer } from "components/Footer";
+import headerPic from "../assets/Headerpic.jpeg";
 
 const LOGIN_URL = "https://bouquetdb.herokuapp.com/sessions";
 
 const Wrapper = styled.div`
-  margin-top: 100px;
   display: flex;
   flex-flow: row nowrap;
   align-items: center;
   justify-content: center;
+  background-image: url(${headerPic});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-color: pink;
 `;
 
 const Form = styled.form`
@@ -37,9 +43,9 @@ const Input = styled.input`
   border: none;
   font-family: "Poppins";
   font-size: 8px;
-  ::placeholder{
-  color: #FF7C90;
-  }: 
+  ::placeholder {
+    color: #ff7c90;
+  }
 `;
 
 const Header = styled.header`
@@ -55,8 +61,13 @@ const Header = styled.header`
 const LoginContainer = styled.div`
   background-color: #fde7ea;
   padding: 20px;
-  margin-bottom: 20px;
+  margin: 20px;
   width: 45%;
+  opacity: 95%;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const Button = styled.button`
@@ -75,65 +86,65 @@ export const LogIn = () => {
   const accessToken = useSelector((store) => store.user.login.accessToken);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [address, setAddress] = useState('');
-  const [zipCode, setZipCode] = useState('');
-  const [city, setCity] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [city, setCity] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
   const errorMessage = useSelector((store) => store.user.login.errorMessage);
-
 
   const handleLogin = (event) => {
     event.preventDefault();
     dispatch(login(email, password));
   };
 
-
   if (!accessToken) {
-
     return (
-      <Wrapper>
-        {errorMessage && <h4>Error Message : {`${errorMessage}`}</h4>}
-        <LoginContainer>
-          <Form>
-            <Header>Logga in</Header>
-            <Label>Email</Label>
-            <Input
-              type="text"
-              required
-              placeholder="dittnamn@gmail.com"
-              value={email}
-              onChange={(event) => setEmail(event.target.value)}
-            />
-            <Label>Password</Label>
-            <Input
-              type="password"
-              required
-              placeholder="********"
-              value={password}
-              onChange={(event) => setPassword(event.target.value)}
-            />
+      <>
+        <NavBar />
+        <Wrapper>
+          {errorMessage && <h4>Error Message : {`${errorMessage}`}</h4>}
+          <LoginContainer>
+            <Form>
+              <Header>Logga in</Header>
+              <Label>Email</Label>
+              <Input
+                type="text"
+                required
+                placeholder="dittnamn@gmail.com"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+              />
+              <Label>Password</Label>
+              <Input
+                type="password"
+                required
+                placeholder="********"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              />
 
+              <Button type="submit" onClick={handleLogin}>
+                Logga in
+              </Button>
 
-            <Button type="submit" onClick={handleLogin}>
-              Logga in
-            </Button>
-
-            <Link
-              to={"/signup/"}
-              style={{
-                fontFamily: "Poppins",
-                fontSize: "12px",
-                color: "#ff7c98",
-                textDecoration: "none",
-              }}
-            >
-              Registrera dig
-            </Link>
-          </Form>
-        </LoginContainer>
-      </Wrapper>
+              <Link
+                to={"/signup/"}
+                style={{
+                  fontFamily: "Poppins",
+                  fontSize: "12px",
+                  color: "#ff7c98",
+                  textDecoration: "none",
+                }}
+              >
+                Registrera dig
+              </Link>
+            </Form>
+          </LoginContainer>
+        </Wrapper>
+        <Footer />
+      </>
     );
   } else {
     return <OrderSummary />;
