7d229c47bebe651379d393889626bfdb8666197d
Ruterryter <hanna.ruter@gmail.com>
2020-06-15 15:38:29 +0200

    fecth userdata orderSummary

--- frontend/src/components/OrderSummary.js
+++ frontend/src/components/OrderSummary.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { Link, useHistory } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
@@ -21,6 +21,7 @@ const Button = styled.button`
 `
 
 export const OrderSummary = () => {
+  const [orders, setOrders] = useState([])
   const dispatch = useDispatch()
   const history = useHistory()
   const userId = useSelector((store) => store.user.login.userId)
@@ -35,14 +36,31 @@ export const OrderSummary = () => {
   const items = cartItems.map((item) => item._id)
   const accessToken = useSelector((store) => store.user.login.accessToken)
 
+
+  //test skicka vidare info om användare men tycker man ska kunna använda use selector och hämta detta från reducerns istället?
+  // const USERS_URL = `https://bouquetdb.herokuapp.com/users${userId}`
+
+  // useEffect(() => {
+  //   fetch(USERS_URL, {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: accessToken,
+  //     },
+  //   })
+  //     .then((res) => res.json())
+  //     .then((json) => {
+  //       setOrders(json.orderHistory)
+  //     })
+  // }, [accessToken, USERS_URL])
+
   const handleSubmit = () => {
     dispatch(submitOrder(items, userId, firstName, address, lastName, email, zipCode, city, phoneNumber, accessToken))
     history.push("/confirmation")
   }
-
+  // behöver man skicka med en key för din order här? och är det rätt key i så fall?
   return (
     <>
-      <h2>{`Din beställning: ${cartItems[0].name} ${cartItems[0].price} kr.`} </h2>
+      <h2 key={orders._id}>{`Din beställning: ${cartItems[0].name} ${cartItems[0].price} kr.`} </h2>
       <h2> Dina leveransuppgifter:</h2>
       <p>{firstName} {lastName}</p>
       <p>{address}</p>
