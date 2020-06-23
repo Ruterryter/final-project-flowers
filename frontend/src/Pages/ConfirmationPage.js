6fd265e701457a5f106cad9fef1f6252483676a6
Elin Aarenstrup <hej@elinaarenstrup.com>
2020-06-23 12:39:31 +0200

    changed vh on confirmation page

--- frontend/src/Pages/ConfirmationPage.js
+++ frontend/src/Pages/ConfirmationPage.js
@@ -7,7 +7,7 @@ import { StartPage } from "Pages/StartPage.js";
 
 const CardWrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: 80vh;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
