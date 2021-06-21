b4bba2d1450c21535407ad8399f229d88c3e79cc
Hanna Ruter <hanna.ruter@gmail.com>
2021-06-21 10:35:52 +0200

    updating redux beacon code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,3 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import { App } from './App';
-
-// import GA4React from 'ga-4-react';
-// const ga4react = new GA4React('G-271954971');
-
-// (async (_) => {
-//   await ga4react.initialize();
-
-//   ReactDOM.render(<App />, document.getElementById('root'));
-// })();
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
